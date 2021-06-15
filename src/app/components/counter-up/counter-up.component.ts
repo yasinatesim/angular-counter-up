6370557cd40809a84dd0f9359715bc0a2072289e
Yasin ATEŞ <yasinatesim@gmail.com>
2021-06-15 21:01:07 +0300

    feat: add counter up component

---
+++ src/app/components/counter-up/counter-up.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-up',
+  templateUrl: './counter-up.component.html',
+  styleUrls: ['./counter-up.component.scss']
+})
+export class CounterUpComponent implements OnInit {
+  counter: number = 0
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  increase() {
+
+  }
+
+  decrease() {
+
+  }
+
+  reset() {
+
+  }
+
+}
