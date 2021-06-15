42387f6b4bf42abfafcee29cd1a39e023fae50a5
Yasin ATEŞ <yasinatesim@gmail.com>
2021-06-15 21:00:35 +0300

    chore: add app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterUpComponent } from './components/counter-up/counter-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterUpComponent
   ],
   imports: [
     BrowserModule
